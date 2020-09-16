c26dddb3656ce2ab8a5450ea1bb5579aa25fcf30
jongseop-park <jongseop94@adcapsule.co.kr>
2020-09-16 10:10:37 +0900

    최초 생성

---
+++ src/main/java/com/pbboard/PbboardApplication.java
@@ -0,0 +1,13 @@
+package com.pbboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbboardApplication.class, args);
+    }
+
+}
